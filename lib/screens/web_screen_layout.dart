473617e799f8e09fb1b241848664500623359e89
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-12-31 18:08:11 +0545

    web screen chat screen

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../colors.dart';
+import '../widgets/chat_list.dart';
 import '../widgets/contacts_list.dart';
+import '../widgets/web_chat_appbar.dart';
 import '../widgets/web_profile_bar.dart';
 import '../widgets/web_search_bar.dart';
 
@@ -16,12 +18,12 @@ class WebScreenLayout extends StatelessWidget {
         children: [
           const Expanded(
             child: SingleChildScrollView(
-              //left side column which contains webprofilebar,search bar 
+              //left side column which contains webprofilebar,search bar
               //and contactlist in a column
               child: Column(
                 children: [
                   WebProfileBar(),
-                   WebSearchBar(),
+                  WebSearchBar(),
                   ContactsList(),
                 ],
               ),
@@ -41,7 +43,76 @@ class WebScreenLayout extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
-          )
+            child: Column(
+              children: [
+                //its a web chat app bar
+                const ChatAppBar(),
+                const SizedBox(height: 20),
+                const Expanded(
+                  child: ChatList(),
+                ),
+                //message input box
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: dividerColor),
+                    ),
+                    color: chatBarMessage,
+                  ),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.emoji_emotions_outlined,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.attach_file,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 10,
+                            right: 15,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: searchBarColor,
+                              hintText: 'Type a message',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                                borderSide: const BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.only(left: 20),
+                            ),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.mic,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/widgets/web_search_bar.dart
+++ lib/widgets/web_search_bar.dart
@@ -16,6 +16,25 @@ class WebSearchBar extends StatelessWidget {
           bottom: BorderSide(color: dividerColor),
         ),
       ),
+      child: TextField(
+          decoration: InputDecoration(
+        filled: true,
+        fillColor: searchBarColor,
+        prefixIcon: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Icon(Icons.search, size: 20),
+        ),
+        hintStyle: const TextStyle(fontSize: 14),
+        hintText: 'Search or start new chat',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+          borderSide: const BorderSide(
+            width: 0,
+            style: BorderStyle.none,
+          ),
+        ),
+        contentPadding: const EdgeInsets.all(10),
+      )),
     );
   }
 }
